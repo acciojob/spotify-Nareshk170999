030c4fda79886ee8c42f046cab1c363f707d00b3
NareshKumar K <naresh170999@gmail.com>
2023-09-05 22:30:46 +0530

    try 1

--- src/main/java/com/driver/SpotifyRepository.java
+++ src/main/java/com/driver/SpotifyRepository.java
@@ -38,23 +38,85 @@ public class SpotifyRepository {
     }
 
     public User createUser(String name, String mobile) {
+        User user = new User(name,mobile);
+        users.add(user);
+        return user;
     }
 
     public Artist createArtist(String name) {
+        Artist artist = new Artist(name);
+        artists.add(artist);
+        return artist;
     }
 
     public Album createAlbum(String title, String artistName) {
+        Artist artist1 = null;
+        for (Artist artist : artists){
+            if(artist.getName() == artistName){
+                artist1 = artist;
+                break;
+            }
+        }
+        if(artist1 ==  null){
+            artist1 = createArtist(artistName);
+            Album album = new Album(title);
+            albums.add(album);
+            List<Album>list = new ArrayList<>();
+            list.add(album);
+            artistAlbumMap.put(artist1,list);
+            return album;
+        }else{
+            Album album = new Album(title);
+            albums.add(album);
+            if (albumSongMap.containsKey(artist1)== true){
+                List<Album> albumList = artistAlbumMap.get(artist1);
+                albumList.add(album);
+                artistAlbumMap.put(artist1,albumList);
+            }else{
+                List<Album>albumList = new ArrayList<>();
+                albumList.add(album);
+                artistAlbumMap.put(artist1,albumList);
+            }
+            return album;
+        }
+
     }
 
     public Song createSong(String title, String albumName, int length) throws Exception{
+        Album album = null;
+        for(Album album1 : albums){
+            if(album1.getTitle() == albumName){
+                album = album1;
+                break;
+            }
+        }
+        if (album == null){
+            throw new Exception("Album does not exist");
+        }else{
+            Song song = new Song(title,length);
+            songs.add(song);
+            if (albumSongMap.containsKey(album)){
+                List<Song> l = albumSongMap.get(album);
+                l.add(song);
+                albumSongMap.put(album,l);
+            }else {
+                List<Song> songList = new ArrayList<>();
+                songList.add(song);
+                albumSongMap.put(album,songList);
+            }
+            return song;
+        }
     }
 
     public Playlist createPlaylistOnLength(String mobile, String title, int length) throws Exception {
-
+        Playlist playlist = new Playlist(title);
+        int totalDuration =0 ;
+        for (Song song : user.g)
     }
 
     public Playlist createPlaylistOnName(String mobile, String title, List<String> songTitles) throws Exception {
-
+        Playlist playlist = new Playlist(mobile,title,songTitles);
+        playlists.add(playlist);
     }
 
     public Playlist findPlaylist(String mobile, String playlistTitle) throws Exception {
@@ -69,5 +131,6 @@ public class SpotifyRepository {
     }
 
     public String mostPopularSong() {
+
     }
 }
